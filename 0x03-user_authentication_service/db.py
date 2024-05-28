e4b00f139f6f14d7270ebaaae0e02bed82dad832
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-05-28 13:21:29 +0100

    new add_user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -44,10 +44,13 @@ class DB:
             The user object
         '''
 
-        user = User(email=email, hashed_password=hashed_password)
-
-        self._session.add(user)
-        self._session.commit()
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+        except Exception:
+            self._session.rollback()
+            user = None
         return user
 
     def find_user_by(self, **kwargs: Dict[str, Any]) -> User:
